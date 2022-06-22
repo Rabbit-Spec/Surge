15cce1e073a7c559b4e48c5eb470d7f2d958ea66
Rabbit-Spec <59154499+Rabbit-Spec@users.noreply.github.com>
2022-06-22 12:30:47 +0800

    Update Timecard.js

--- Panel/Timecard/Timecard.js
+++ Panel/Timecard/Timecard.js
@@ -96,7 +96,7 @@ function icon_now(num){
 $done({
 title:title_random(tnumcount(Number(nowlist))),
 icon:icon_now(tnumcount(Number(nowlist))),
-content:tlist[nowlist][0]+":"+today(tnumcount(nowlist))+"，"+tlist[Number(nowlist) + Number(1)][0] +":"+ tnumcount(Number(nowlist) + Number(1))+ "天,"+tlist[Number(nowlist) + Number(2)][0]+":"+tnumcount(Number(nowlist) + Number(2))+"天"
+content:tlist[nowlist][0]+":"+today(tnumcount(nowlist))+","+tlist[Number(nowlist) + Number(1)][0] +":"+ tnumcount(Number(nowlist) + Number(1))+ "天,"+tlist[Number(nowlist) + Number(2)][0]+":"+tnumcount(Number(nowlist) + Number(2))+"天"
 })
 
 function title_random(num){
