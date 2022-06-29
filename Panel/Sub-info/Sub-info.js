492e84f2d076535eac2d9879a17a1169e87c26b7
Rabbit-Spec <59154499+Rabbit-Spec@users.noreply.github.com>
2022-06-29 11:21:11 +0800

    小更新

--- Panel/Sub-info/Sub-info.js
+++ Panel/Sub-info/Sub-info.js
@@ -4,32 +4,6 @@
  * 由@Rabbit-Spec修改
  * 更新日期：2022.06.29
  * 版本：1.7
-
-示例↓↓↓ 
-----------------------------------------
-
-[Script]
-Sub_info = type=generic,timeout=10,script-path=https://raw.githubusercontent.com/mieqq/mieqq/master/sub_info_panel.js,script-update-interval=0,argument=url=[URL encode 后的机场节点链接]&reset_day=1&title=AmyInfo&icon=bonjour&color=#007aff
-
-[Panel]
-Sub_info = script-name=Sub_info,update-interval=600
-
-----------------------------------------
-
-先将带有流量信息的节点订阅链接encode，用encode后的链接替换"url="后面的[机场节点链接]
-
-（实在不会可以用这个捷径生成panel和脚本，https://www.icloud.com/shortcuts/3f24df391d594a73abd04ebdccd92584）
-
-可选参数 &reset_day，后面的数字替换成流量每月重置的日期，如1号就写1，8号就写8。如"&reset_day=8",不加该参数不显示流量重置信息。
-
-可选参数 &expire，机场链接不带expire信息的，可以手动传入expire参数，如"&expire=2022-02-01",注意一定要按照yyyy-MM-dd的格式。不希望显示到期信息也可以添加&expire=false取消显示。
-
-可选参数"title=xxx" 可以自定义标题。
-
-可选参数"icon=xxx" 可以自定义图标，内容为任意有效的 SF Symbol Name，如 bolt.horizontal.circle.fill，详细可以下载app https://apps.apple.com/cn/app/sf-symbols-browser/id1491161336
-
-可选参数"color=xxx" 当使用 icon 字段时，可传入 color 字段控制图标颜色，字段内容为颜色的 HEX 编码。如：color=#007aff
-----------------------------------------
 */
 
 let args = getArgs();
