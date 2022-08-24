65d80a19d1e0d47c2434ea134abb43aed4e4379f
Rabbit-Spec <59154499+Rabbit-Spec@users.noreply.github.com>
2022-08-24 15:11:29 +0800

    修复了一个小问题

--- Module/Panel/Sub-info/Moore/Sub-info.js
+++ Module/Panel/Sub-info/Moore/Sub-info.js
@@ -2,8 +2,8 @@
  * 由@mieqq编写
  * 原脚本地址：https://raw.githubusercontent.com/mieqq/mieqq/master/sub_info_panel.js
  * 由@Rabbit-Spec修改
- * 更新日期：2022.06.11
- * 版本：1.3
+ * 更新日期：2022.08.24
+ * 版本：1.5
 
 示例↓↓↓ 
 ----------------------------------------
